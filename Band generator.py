407c6db2f0e8d9fc92ac533005591013ea34a905
SREELAKSHMISUD <121602892+SREELAKSHMISUD@users.noreply.github.com>
2024-07-04 01:31:37 +0530

    Add files via upload

---
+++ Band generator.py
@@ -0,0 +1,15 @@
+#1. Create a greeting
+print("Hey All Welcome to Band generator")
+
+
+#2. Asking the user for the city that they grew up in.\n the input cursor shows on a new line
+ask = input("What is the name of city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+ask2 = input("what is the name of your pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + ask +" " + ask2)
+
+
+
